b0f1183a1d115717237dd36025c312479e64f626
Lucas Santos <leos9877@gmail.com>
2022-05-23 22:29:50 -0300

    change archive for deploy

--- Configurations/settings.py
+++ Configurations/settings.py
@@ -1,20 +1,17 @@
 import os
 import os, sys
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(
     os.path.join(BASE_DIR, "Apps")
 )
 
+SECRET_KEY = str(os.environ.get('SECRET_KEY', "ddwr&@m^@k#bh+py-r@kgff4u!h7j%nvhja(^is*1h4l!4oaq8"))
 
-SECRET_KEY = 'ddwr&@m^@k#bo%+d+py-r@kgfo4u!7j%nv@(^is*14l!4coaq8'
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-
-DEBUG = True
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -30,7 +27,9 @@ INSTALLED_APPS +=[
     'Store',
     'smart_selects',
 ]
+
 USE_DJANGO_JQUERY = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -60,15 +59,24 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Configurations.wsgi.application'
+
 AUTH_USER_MODEL = "Users.Profile" 
+
 LOGIN_REDIRECT_URL = 'LadingPage'
+
 LOGIN_URL = 'ViewLogin'
+
 # LOGOUT_URL = ''
 # LOGOUT_REDIRECT_URL = ''
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': str(os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3')),
+        'NAME': str(os.environ.get('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3'))),
+        'USER': str(os.environ.get('DB_USER', '')),
+        'PASSWORD': str(os.environ.get('DB_PASSWORD', '')),
+        'HOST': str(os.environ.get('DB_HOST', '')),
+        'PORT': str(os.environ.get('DB_PORT', ''))
     }
 }
 
@@ -87,19 +95,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LANGUAGE_CODE = 'pt-br'
+
 TIME_ZONE = 'America/Sao_Paulo'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "StaticFile")
+    os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL='/media/'
-MEDIA_ROOT='media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
