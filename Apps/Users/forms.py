15d5929a6790d840c673e926519cd8c576c3a5e1
Lucas Santos <leos9877@gmail.com>
2022-05-23 22:41:49 -0300

    :bug: testing deploy

--- Apps/Users/forms.py
+++ Apps/Users/forms.py
@@ -5,15 +5,15 @@ from Users.models import Profile
 
 class UserForm(forms.ModelForm):
 
-    def save(self, commit=True):
-        user = super(UserForm, self).save(commit=False)
-        user.set_password(self.cleaned_data['password'])
-        user.is_active = True
-        user.is_staff = False
-        user.is_superuser = False
-        if commit:
-            user.save()
-        return user
+    # def save(self, commit=True):
+    #     user = super(UserForm, self).save(commit=False)
+    #     user.set_password(self.cleaned_data['password'])
+    #     user.is_active = True
+    #     user.is_staff = False
+    #     user.is_superuser = False
+    #     if commit:
+    #         user.save()
+    #     return user
 
     
     class Meta:
